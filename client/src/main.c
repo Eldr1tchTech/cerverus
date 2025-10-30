e2319a127fc9f222546be7ee3ddf41e69711fb2f
Eldr1tchTech <eldr1tchtech.dev@gmail.com>
2025-10-29 18:45:07 -0600

    simplest version without socket wrapper, can send and get between cleint and server

--- client/src/main.c
+++ client/src/main.c
@@ -1,15 +1,26 @@
 #include <core/logger.h>
-#include <core/cmemory.h>
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
-int main () {
-    cmem_print_stats();
-    char* block = cmem_allocate(MEMORY_TAG_STRING, sizeof(char) * 8);
+int main() {
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     
-    strcpy(block, "Hello!");
-
-    cmem_print_stats();
-
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8080),
+        .sin_addr = { 0 }
+    };
+    
+    connect(sock_fd, (struct sockaddr*)&addr, sizeof(addr));
+    send(sock_fd, "GET", strlen("GET"), 0);
+    
+    char buffer[256] = {0};
+    recv(sock_fd, buffer, 256, 0);
+    LOG_DEBUG("Received: %s", buffer);
+    
+    close(sock_fd);
     return 0;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -1,15 +1,33 @@
 #include <core/logger.h>
-#include <core/cmemory.h>
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
-int main () {
-    cmem_print_stats();
-    char* block = cmem_allocate(MEMORY_TAG_STRING, sizeof(char) * 8);
+int main() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8080),
+        .sin_addr = { 0 }
+    };
     
-    strcpy(block, "Hello!");
+    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 10);
 
-    cmem_print_stats();
+    int client_fd = accept(server_fd, 0, 0);
 
+    char buffer[256] = {0};
+    recv(client_fd, buffer, 256, 0);
+    LOG_DEBUG("Received: %s", buffer);
+    
+    if (strncmp(buffer, "GET", 3) == 0) {
+        send(client_fd, "POST", strlen("POST"), 0);
+    }
+    
+    close(client_fd);
+    close(server_fd);
     return 0;
 }
